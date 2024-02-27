35cdb7a7d5d2050810fb97e2b2ae5fe0f5df5ab7
Go Away <baltzer.matt@gmail.com>
2024-02-26 21:51:47 -0800

    Created the tile class, udpated the main page to view the tile

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame, sys
+from settings import *
+from tiles import Tile
 
 # Main game setup
 pygame.init()
-screen_width = 1200
-screen_height = 800
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
+
+# Sprite group
+test_tile = pygame.sprite.Group(Tile(100,100),200)
 
 # Loop for checking if you quit the game
 while True:
@@ -16,7 +19,7 @@ while True:
 
     # When quitting the game, fill the screen black
     screen.fill('black')
-
+    test_tile.draw(screen)
     # Sets the framerate for the gakme
     pygame.display.update()
     clock.tick(60)
