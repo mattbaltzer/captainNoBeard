54bb2623a203d5eb1b53ff025fe52c1c45415fa6
Go Away <baltzer.matt@gmail.com>
2024-03-01 17:44:57 -0800

    Created the player, has basic left and right movement. Updated the level tileset to have the player starting point

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
 from settings import tile_size
+from player import Player
 
 class Level:
     def __init__(self,level_data,surface):
@@ -11,15 +12,28 @@ class Level:
 
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         # Loop to place the tiles on the game screen
         for row_index,row in enumerate(layout):
             for column_index,column in enumerate(row):
+                x = column_index * tile_size
+                y = row_index * tile_size
+
                 if column == 'X':
-                    x = column_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x,y),tile_size)
                     self.tiles.add(tile)
+                if column == 'P':
+                    player_sprite = Player((x,y))
+                    self.player.add(player_sprite)
+        
+
 
     def run(self):
+        # Drawing the level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        # Drawing the player tiles
+        self.player.update()
+        self.player.draw(self.display_surface)
