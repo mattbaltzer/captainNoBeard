9eceedf95b7dd08c9ad57a811e62d55f259da8fc
Go Away <baltzer.matt@gmail.com>
2024-03-11 17:22:15 -0700

    Finished player camera scrolling based upon the size of the window

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 from player import Player
 
 class Level:
@@ -32,9 +32,15 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < 200:
+        if player_x < screen_width / 4 and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
 
     def run(self):
         # Drawing the level tiles
