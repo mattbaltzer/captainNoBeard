8cbb20aa1b3837567212d3721e0c10b7e392b1c8
Go Away <baltzer.matt@gmail.com>
2024-03-14 18:21:21 -0700

    Updated the player sprite to play a running animation.

--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 import pygame
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos):
         super().__init__()
-        self.image = pygame.Surface((32,64))
-        self.image.fill('red')
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
 
         # Player movement
@@ -14,8 +17,22 @@ class Player(pygame.sprite.Sprite):
         self.jump_speed = -16
 
     def import_character_assets(self):
-        character_path = '../graphics/character/'
+        character_path = './graphics/character/'
         self.animations = {'idle':[],'run':[],'jump':[],'fall':[]}
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation 
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations['run']
+
+        # Loop over the frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -40,4 +57,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.animate()
         
