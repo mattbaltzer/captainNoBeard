190787fbe906928ed07da5349701e21109fee4ed
Go Away <baltzer.matt@gmail.com>
2024-03-18 16:55:17 -0700

    Changed the player animation to flip left and right. Checking to see if the player is colliding with the level. Need to properly place the player on the level.

--- player.py
+++ player.py
@@ -18,6 +18,11 @@ class Player(pygame.sprite.Sprite):
 
         # Sets up the Player status
         self.status = 'idle'
+        self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
 
     def import_character_assets(self):
         character_path = './graphics/character/'
@@ -35,7 +40,22 @@ class Player(pygame.sprite.Sprite):
         if self.frame_index >= len(animation):
             self.frame_index = 0
 
-        self.image = animation[int(self.frame_index)]
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = animation[int(self.frame_index)]
+        else:
+            # Checks if the image flipped on the X or Y axis 
+            flipped_image = pygame.transform.flip(image,True,False)
+            self.image = flipped_image
+
+        # Setting the rectangle
+        if self.on_ground:
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+        elif self.on_ceiling:
+            self.rect = self.image.get_rect(midtop = self.rect.midtop)
+        else:
+            self.rect = self.image.get_rect(center = self.rect.center)
+
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -43,18 +63,20 @@ class Player(pygame.sprite.Sprite):
         # Checks to see if keys were pressed and assign movement to them
         if keys[pygame.K_d]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_a]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
     
     def get_status(self):
         if self.direction.y < 0:
             self.status = 'jump'
-        elif self.direction.y > 0:
+        elif self.direction.y > 1:
             self.status = 'fall'
         else:
             if self.direction.x != 0:
