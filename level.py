4af570f36fbd611051241a42fcc718dc0460763f
Go Away <baltzer.matt@gmail.com>
2024-03-21 17:05:41 -0700

    Fixed the player animation clipping on the level. Fixed a player related direction variable.

--- level.py
+++ level.py
@@ -58,9 +58,9 @@ class Level:
                     player.on_right = True
                     self.current_x = player.rect.right
 
-        if player.on_left and (player.rect.left < self.current_x or player.direction_x >= 0):
+        if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
             player.on_left = False
-        if player.on_right and (player.rect.right > self.current_x or player.direction_x <= 0):
+        if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
             player.on_right = False
 
     def vertical_movement_collision(self):
