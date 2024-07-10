dd6a2fd3c38627bb316a51678a1efe6711bf496f
Go Away <baltzer.matt@gmail.com>
2024-07-10 10:20:06 -0700

    Created the basic framework for the intro level inside the leveldata folder

---
+++ levels/leveldata/level.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Level:
+    def __init__(self,level_data,surface):
+        self.display_surface = surface
+    
+    def run(self):
+        # runs the entire level
+        pass
