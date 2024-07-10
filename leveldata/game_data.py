6c862fe2e4576f978959956ca9e2c0372acf10d2
Go Away <baltzer.matt@gmail.com>
2024-07-10 11:08:11 -0700

    Fixed the pathing issue in regards to reading the csv files

--- leveldata/game_data.py
+++ leveldata/game_data.py
@@ -1,11 +1,11 @@
 level_0 = {
-    'terrain': '../levels/0/intro_level_terrain.csv',
-    'coins': '../levels/0/intro_level_coins.csv',
-    'fg palms': '../levels/0/intro_level_fg_palms.csv',
-    'bg palms': '../levels/0/intro_level_bg_palms.csv',
-    'crates': '../levels/0/intro_level_crates.csv',
-    'enemies': '../levels/0/intro_level_enemies.csv',
-    'constraints': '../levels/0/intro_level_constraints.csv',
-    'player': '../levels/0/intro_level_player.csv',
-    'grass': '../levels/0/intro_level_grass.csv'
+    'terrain': './levels/0/intro_level_terrain.csv',
+    'coins': './levels/0/intro_level_coins.csv',
+    'fg palms': './levels/0/intro_level_fg_palms.csv',
+    'bg palms': './levels/0/intro_level_bg_palms.csv',
+    'crates': './levels/0/intro_level_crates.csv',
+    'enemies': './levels/0/intro_level_enemies.csv',
+    'constraints': './levels/0/intro_level_constraints.csv',
+    'player': './levels/0/intro_level_player.csv',
+    'grass': './levels/0/intro_level_grass.csv'
 }
