d286a98c8ae0d27ff0e57de000045ab3ae80d668
Go Away <baltzer.matt@gmail.com>
2024-07-09 18:03:10 -0700

    Updated the character size and size of the background objects/tiles

---
+++ levels/tilesets/terrain tileset32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="terrain tileset" tilewidth="32" tileheight="32" tilecount="85" columns="17">
+ <image source="../../Treasure Hunters/Palm Tree Island/Sprites/Terrain/Terrain (32x32).png" width="544" height="160"/>
+</tileset>
