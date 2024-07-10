d983ff0d98b8fa2f80419c5547535d9d6ff9a582
Go Away <baltzer.matt@gmail.com>
2024-07-09 21:28:49 -0700

    Changed around some files, updated different sprites, completed the intro level map

--- levels/tilesets/terrain tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="terrain tiles" tilewidth="64" tileheight="64" tilecount="16" columns="8">
- <image source="../../Treasure Hunters/Palm Tree Island/Sprites/Terrain/Terrain (32x32).png" width="544" height="160"/>
-</tileset>
