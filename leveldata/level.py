58ebd51a428b53445ef927d57c5f8d0a1b62630b
Go Away <baltzer.matt@gmail.com>
2024-07-10 18:02:06 -0700

    Added grass, crates, and coins to the level. Animated the coins and made it so both gold and silver coins would be displayed

--- leveldata/level.py
+++ leveldata/level.py
@@ -1,17 +1,29 @@
 import pygame
 from support import import_csv_layout, import_cut_graphics
 from settings import tile_size
-from tiles import Tile
+from tiles import Tile, StaticTile, Crate, Coin
 
 class Level:
     def __init__(self,level_data,surface):
         # Generates the surface and controls the level scroll
         self.display_surface = surface
-        self.world_shift = 0
+        self.world_shift = -3
 
         # Creates the terrain by seleting values from the terrain csv
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
+
+        # Creates the grass by seleting values from the grass csv
+        grass_layout = import_csv_layout(level_data['grass'])
+        self.grass_sprites = self.create_tile_group(grass_layout,'grass')
+        
+        # Creates the crates setup
+        crate_layout = import_csv_layout(level_data['crates'])
+        self.crate_sprites = self.create_tile_group(crate_layout,'crates')
+
+        # Creates the animated coins setup
+        coin_layout = import_csv_layout(level_data['coins'])
+        self.coin_sprites = self.create_tile_group(coin_layout,'coins')
     
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -24,12 +36,42 @@ class Level:
 
                     if type == 'terrain':
                         terrain_tile_list = import_cut_graphics('./graphics/terrain/terrain_tiles.png')
-                        sprite = Tile(tile_size,x,y)
-                        sprite_group.add(sprite)
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size,x,y,tile_surface)
+
+                    if type == 'grass':
+                        grass_tile_list = import_cut_graphics('./graphics/decoration/grass/grass.png')
+                        tile_surface = grass_tile_list[int(val)]
+                        sprite = StaticTile(tile_size,x,y,tile_surface)
+                    
+                    if type == 'crates':
+                        sprite = Crate(tile_size,x,y)
+
+                    if type == 'coins':
+                        if val == '0':
+                            sprite = Coin(tile_size,x,y,'./graphics/coins/gold')
+                        elif val == '1':
+                            sprite = Coin(tile_size,x,y,'./graphics/coins/silver')
+                    
+                    
+
+                    sprite_group.add(sprite)
 
         return sprite_group
     
     def run(self):
         # runs the entire level
-        self.terrain_sprites.draw(self.display_surface)
         self.terrain_sprites.update(self.world_shift)
+        self.terrain_sprites.draw(self.display_surface)
+
+        # grass
+        self.grass_sprites.update(self.world_shift)
+        self.grass_sprites.draw(self.display_surface)
+
+        # crates
+        self.crate_sprites.update(self.world_shift)
+        self.crate_sprites.draw(self.display_surface)
+
+        #coins
+        self.coin_sprites.update(self.world_shift)
+        self.coin_sprites.draw(self.display_surface)
--- leveldata/support.py
+++ leveldata/support.py
@@ -1,6 +1,18 @@
 import pygame
+from os import walk
 from csv import reader
 from settings import tile_size
+
+def import_folder(path):
+    surface_list = []
+
+    for _,__,image_files in walk(path):
+        for image in image_files:
+            full_path = path + '/' + image
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+
+    return surface_list
 
 def import_csv_layout(path):
     terrain_map = []
@@ -21,7 +33,7 @@ def import_cut_graphics(path):
             x = col * tile_size
             y = row * tile_size
             new_surf = pygame.Surface((tile_size,tile_size))
-            new_surf.blit(surface,(x,y),pygame.Rect(x,y,tile_size,tile_size))
+            new_surf.blit(surface,(0,0),pygame.Rect(x,y,tile_size,tile_size))
             cut_tiles.append(new_surf)
 
     return cut_tiles
