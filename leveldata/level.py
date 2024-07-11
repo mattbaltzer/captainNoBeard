6bfce04ac0774e28e4dff3e1cfe885ae463735e2
Go Away <baltzer.matt@gmail.com>
2024-07-11 15:48:10 -0700

    Created the sky, water and cloud background images

--- leveldata/level.py
+++ leveldata/level.py
@@ -1,15 +1,22 @@
 import pygame
 import random
 from support import import_csv_layout, import_cut_graphics
-from settings import tile_size
+from settings import tile_size, screen_height
 from tiles import Tile, StaticTile, Crate, Coin, Palm
 from enemy import Enemy
+from decoration import Sky, Water, Clouds
 
 class Level:
     def __init__(self,level_data,surface):
         # Generates the surface and controls the level scroll
         self.display_surface = surface
-        self.world_shift = -1
+        self.world_shift = -2
+
+        # Player setup
+        player_layout = import_csv_layout(level_data['player'])
+        self.player = pygame.sprite.GroupSingle()
+        self.goal = pygame.sprite.GroupSingle()
+        self.player_setup(player_layout)
 
         # Creates the terrain by seleting values from the terrain csv
         terrain_layout = import_csv_layout(level_data['terrain'])
@@ -42,6 +49,12 @@ class Level:
         # Constraint
         constraint_layout = import_csv_layout(level_data['constraints'])
         self.constraint_sprites = self.create_tile_group(constraint_layout,'constraints')
+
+        # Skybox/Decorations
+        self.sky = Sky(8)
+        level_width = len(terrain_layout[0]) * tile_size
+        self.water = Water(screen_height - 30,level_width)
+        self.clouds = Clouds(400,level_width,30)
     
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -90,6 +103,19 @@ class Level:
 
         return sprite_group
     
+    def player_setup(self,layout):
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+                if val == '0':
+                    print('player goes here')
+                elif val == '1':
+                    hat_surface = pygame.image.load('./graphics/character/hat.png').convert_alpha()
+                    sprite = StaticTile(tile_size,x,y,hat_surface)
+                    self.goal.add(sprite)
+
+
     def enemy_collision_reverse(self):
         for enemy in self.enemy_sprites.sprites():
             # The false prevents the enemy from being killed when a collision happens
@@ -97,6 +123,10 @@ class Level:
                 enemy.reverse()
 
     def run(self):
+        # skybox/decorations
+        self.sky.draw(self.display_surface)
+        self.clouds.draw(self.display_surface, self.world_shift)
+
         # background palms
         self.bg_palm_sprites.update(self.world_shift)
         self.bg_palm_sprites.draw(self.display_surface)
@@ -126,3 +156,11 @@ class Level:
         # foreground palms
         self.fg_palm_sprites.update(self.world_shift)
         self.fg_palm_sprites.draw(self.display_surface)
+
+        # player sprites
+        self.goal.update(self.world_shift)
+        self.goal.draw(self.display_surface)
+
+        # water sprites
+        self.water.draw(self.display_surface,self.world_shift)
+
